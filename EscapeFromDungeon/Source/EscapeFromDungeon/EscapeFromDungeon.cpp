b92f02d82c909aa7b4d06be43058468fc5fa87ef
MaciejTarkowski <mtarkowski46@gmail.com>
2023-01-10 21:17:38 +0100

    EscapeFromDungeon: Initial commit part 3

---
+++ EscapeFromDungeon/Source/EscapeFromDungeon/EscapeFromDungeon.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EscapeFromDungeon.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EscapeFromDungeon, "EscapeFromDungeon" );
+ 
